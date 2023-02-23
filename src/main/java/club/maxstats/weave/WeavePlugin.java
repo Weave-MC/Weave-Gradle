a3706f09f95cf954711be1fcb1dc0062391c4c84
Scherso <Scherso@users.noreply.github.com>
2023-02-22 20:52:43 -0500

    Add template tasks

--- src/main/java/club/maxstats/weave/WeavePlugin.java
+++ src/main/java/club/maxstats/weave/WeavePlugin.java
@@ -5,12 +5,24 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.JavaPlugin;
 
+/**
+ * TODO: Add description here.
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ * @author Scherso (<a href="https://github.com/Scherso">...</a>), Max (<a href="https://github.com/exejar">...</a>)
+ *         Nils <3 (<a href="https://github.com/Nilsen84">...</a>)
+ */
 public class WeavePlugin implements Plugin<Project> {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 
+    /**
+     * {@link Plugin#apply(Object)}
+     * @param target The target object
+     */
     @Override
     public void apply(@NonNull Project target) {
         target.getPluginManager().apply(JavaPlugin.class);
---
+++ src/main/java/club/maxstats/weave/task/SetupTask.java
@@ -0,0 +1,15 @@
+package club.maxstats.weave.task;
+
+import org.gradle.api.DefaultTask;
+
+/**
+ * TODO: Add description here.
+ * @author Scherso (<a href="https://github.com/Scherso">...</a>), Max (<a href="https://github.com/exejar">...</a>)
+ */
+public class SetupTask extends DefaultTask {
+
+    public SetupTask() {
+        super();
+    }
+
+}
