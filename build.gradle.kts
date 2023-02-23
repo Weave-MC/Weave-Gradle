7e53487e098890d5a6675869a018056da82f03c5
Scherso <Scherso@users.noreply.github.com>
2023-02-22 20:21:41 -0500

    Resolution on build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,21 +13,18 @@ version = projectVersion
 
 repositories.mavenCentral()
 
-dependencies
-
-<<<<<<< HEAD
 dependencies {
     implementation("org.ow2.asm:asm:9.2")
     implementation("org.ow2.asm:asm-tree:9.2")
     implementation("org.ow2.asm:asm-commons:9.2")
     compileOnly("org.projectlombok:lombok:1.18.22")
     annotationProcessor("org.projectlombok:lombok:1.18.22")
-=======
+}
+
 gradlePlugin {
     /* We gotta figrue this out */
     val loom by plugins.creating {
         id = "$group"
         implementationClass = "${group}.WeavePlugin"
     }
->>>>>>> 9b48e6917f966b8c49d6682bcc212e87ff3b20c1
 }
--- src/main/java/club/maxstats/weave/WeavePlugin.java
+++ src/main/java/club/maxstats/weave/WeavePlugin.java
@@ -1,6 +1,7 @@
 package club.maxstats.weave;
 
 public class WeavePlugin {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
