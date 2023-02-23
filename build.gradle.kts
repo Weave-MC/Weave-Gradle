378dc0ce8b72657c8fdd9006b99ce1c6e369f487
Scherso <Scherso@users.noreply.github.com>
2023-02-22 20:11:38 -0500

    Add WeavePlugin class

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,26 @@
 plugins {
     idea
     java
+    `java-gradle-plugin`
 }
 
 val projectName:    String by project
 val projectVersion: String by project
 val projectGroup:   String by project
 
-group = projectGroup
+group   = projectGroup
 version = projectVersion
 
-repositories {
-    mavenCentral()
+repositories.mavenCentral()
+
+dependencies
+
+gradlePlugin {
+    /* We gotta figrue this out */
+    val loom by plugins.creating {
+        id = "$group"
+        implementationClass = "${group}.WeavePlugin"
+    }
 }
 
-dependencies {
-}
+
