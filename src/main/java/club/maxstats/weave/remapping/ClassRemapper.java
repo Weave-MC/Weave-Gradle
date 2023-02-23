c7ba5c643208f25aaf3a8154ed815e17ab67f620
Scherso <Scherso@users.noreply.github.com>
2023-02-22 21:08:30 -0500

    Add javadocs

--- src/main/java/club/maxstats/weave/remapping/ClassRemapper.java
+++ src/main/java/club/maxstats/weave/remapping/ClassRemapper.java
@@ -2,7 +2,22 @@ package club.maxstats.weave.remapping;
 
 import org.objectweb.asm.commons.Remapper;
 
+/**
+ * Maps obfuscated classes and fields into MCP names using
+ * object-web's {@link Remapper} class, as-well as a mapping file.
+ *
+ * @author Max (<a href="https://github.com/exejar">...</a>)
+ * @see org.objectweb.asm.commons.Remapper
+ */
 public class ClassRemapper extends Remapper {
+
+
+    /**
+     * Maps a class name from obfuscated to MCP.
+     *
+     * @param internalName The internal name of a class.
+     * @return             The mapped name of the class/field.
+     */
     @Override
     public String map(String internalName) {
         String mappedClassName = Mappings.getMappedClassName(internalName);
@@ -12,6 +27,14 @@ public class ClassRemapper extends Remapper {
         return super.map(internalName);
     }
 
+    /**
+     * Maps a method name from obfuscated to MCP.
+     *
+     * @param owner      The internal name of the owner class of the method.
+     * @param name       The name of the method.
+     * @param descriptor The descriptor of the method.
+     * @return           The mapped name of the method.
+     */
     @Override
     public String mapMethodName(String owner, String name, String descriptor) {
         MappedClass mappedClass = Mappings.getMappedClass(owner);
@@ -25,6 +48,14 @@ public class ClassRemapper extends Remapper {
         return super.mapMethodName(owner, name, descriptor);
     }
 
+    /**
+     * Maps a field name from obfuscated to MCP.
+     *
+     * @param owner      The internal name of the owner class of the field.
+     * @param name       The name of the field.
+     * @param descriptor The descriptor of the field.
+     * @return           The mapped name of the field.
+     */
     @Override
     public String mapFieldName(String owner, String name, String descriptor) {
         MappedClass mappedClass = Mappings.getMappedClass(owner);
@@ -37,4 +68,5 @@ public class ClassRemapper extends Remapper {
 
         return super.mapFieldName(owner, name, descriptor);
     }
+
 }
