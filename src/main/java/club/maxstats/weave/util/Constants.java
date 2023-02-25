3584942e284dd7ae71fe4c1185debb1dfb8f9930
exejar <56853437+exejar@users.noreply.github.com>
2023-02-25 15:10:47 -0500

    Created Constants.java Added path constants

---
+++ src/main/java/club/maxstats/weave/util/Constants.java
@@ -0,0 +1,8 @@
+package club.maxstats.weave.util;
+
+import java.io.File;
+
+public class Constants {
+    public static final File WORKING_DIR = new File(".");
+    public static final File CACHE_DIR = new File(WORKING_DIR, ".gradle/minecraft");
+}
