6ead9e9a29665b90ee9d7085b44977af4e189a99
exejar <56853437+exejar@users.noreply.github.com>
2023-02-25 19:15:11 -0500

    Changed CACHE_DIR to global .gradle/caches/weave folder Added MC_CACHE_DIR for 1.8.9 version folder

--- src/main/java/club/maxstats/weave/util/Constants.java
+++ src/main/java/club/maxstats/weave/util/Constants.java
@@ -4,5 +4,6 @@ import java.io.File;
 
 public class Constants {
     public static final File WORKING_DIR = new File(".");
-    public static final File CACHE_DIR = new File(WORKING_DIR, ".gradle/minecraft");
+    public static final File CACHE_DIR = new File(System.getProperty("user.home"), ".gradle/caches/weave");
+    public static final File MC_CACHE_DIR = new File(CACHE_DIR, "1.8.9");
 }
