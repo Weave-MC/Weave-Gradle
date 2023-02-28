e83d0265f090eaa61387b66424b568cfed37aee1
exejar <56853437+exejar@users.noreply.github.com>
2023-02-28 01:23:49 -0500

    Removed unused methods

--- src/main/java/club/maxstats/weave/AbstractPlugin.java
+++ src/main/java/club/maxstats/weave/AbstractPlugin.java
@@ -49,26 +49,4 @@ public class AbstractPlugin implements Plugin<Project> {
         return makeTask(project, name, type);
     }
 
-    protected void configureIDE() {
-        /* For IntelliJ IDEA. */
-//      IdeaModel ideaModel = project.getExtensions().getByType(IdeaModel.class);
-        IdeaModel ideaModel = (IdeaModel) project.getExtensions().getByName("idea");
-        ideaModel.getModule().getExcludeDirs().addAll(project.files(".gradle", "build", ".idea", "out").getFiles());
-        ideaModel.getModule().setDownloadJavadoc(true);
-        ideaModel.getModule().setDownloadSources(true);
-        ideaModel.getModule().setInheritOutputDirs(true);
-//      This is where we'd ad the Minecraft dependency configuration.
-        ideaModel.getModule().getScopes().get("COMPILE").get("plus").add(project.getConfigurations().getByName("MC_DEPENDENCIES"));
-
-        /* For Eclipse. */
-//      EclipseModel eclipseModel = project.getExtensions().getByType(EclipseModel.class);
-        EclipseModel eclipseModel = (EclipseModel) project.getExtensions().getByName("eclipse");
-//      This is where we'd ad the Minecraft dependency configuration.
-        eclipseModel.getClasspath().getPlusConfigurations().add(project.getConfigurations().getByName("MC_DEPENDENCIES"));
-    }
-
-    protected void configureCompilation() {
-
-    }
-
 }
