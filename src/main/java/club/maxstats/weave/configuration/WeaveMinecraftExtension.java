6065ec5c149cb8b23d3f5bf25ced6fd14cda64a2
exejar <56853437+exejar@users.noreply.github.com>
2023-03-04 22:23:20 -0500

    Removed mappings configuration

--- src/main/java/club/maxstats/weave/configuration/WeaveMinecraftExtension.java
+++ src/main/java/club/maxstats/weave/configuration/WeaveMinecraftExtension.java
@@ -3,9 +3,7 @@ package club.maxstats.weave.configuration;
 import org.gradle.api.Project;
 
 public class WeaveMinecraftExtension {
-
     private String version = "";
-    private String mappings = "";
 
     static WeaveMinecraftExtension get(Project project) {
         return (WeaveMinecraftExtension) project.getExtensions().getByName("minecraft");
@@ -14,17 +12,8 @@ public class WeaveMinecraftExtension {
     public String getVersion() {
         return this.version;
     }
-    
-    public String getMappings() {
-        return this.mappings;
-    }
 
     public void setVersion(String version) {
         this.version = version;
     }
-    
-    public void setMappings(String mappings) {
-        this.mappings = mappings;
-    }
-
 }
